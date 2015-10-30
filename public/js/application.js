b78139339c368c71806f51769c706d92323f6631
Matthew LeFurge <email": null>
2015-10-30 16:09:06 -0400

    add ability to add a question and choices

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+
+  $(".create-questions-form .add-choice").on("click", function(event) {
+    event.preventDefault();
+    var button = $(this);
+    var form = button.parents('.create-questions-form');
+    var questionChoiceField = form.find('.question-choice-field:last');
+    questionChoiceField.clone().insertAfter(questionChoiceField);
+    // $(".create-choice").after('<div class="create-choice"></div>');
+    // $(".create-choice").last().attr("method", "post").attr("action","/create_survey/1/create-questions").append('<label class="choice-input" for "choice">Choice:</label>').append('<input class="choice-input" type="text" name="question[choice][]" placeholder="Enter Choice Here"></input>')
+  });
+
 });
